b40bd9e6c02d8fe5bd174921c0c4ffad9493d32c
fhudzinski180 <33383891+fhudzinski180@users.noreply.github.com>
2017-11-13 15:03:00 -0500

    finished copy-userpofile.ps1

--- Copy-UserProfiles.ps1
+++ Copy-UserProfiles.ps1
@@ -1,17 +1,38 @@
+<#
+
+Make sure the account that runs this script has administrative priviledges on both computers
+
+Usage: .\Copy-UserProfiles.ps1 -UserToCopy user1 -RemoteComputer remotepc1 -DomainController dc1
+
+#>
 # Define required variables of the script
 [CmdletBinding()]
 Param (
 
     [Parameter(Mandatory = $True, Position = 1)]
-    [string]$RemoteComputer
+    [string]$UserToCopy,
+    [Parameter(Mandatory = $True, Position = 2)]
+    [string]$RemoteComputer,
+    [Parameter(Mandatory = $True, Position = 3)]
+    [string]$DomainController
 
 )
-$Directory = Get-ChildItem "C:\Users" | Where-Object { $_.Name -eq "fhudzinski" }
 
-if (Test-Path "\\$RemoteComputer\c$\Users\$($Directory.Name)") {
+# We will need the user sid when it comes time to copy the profile
+$UserSID = Get-ADUser -Server $DomainController -Identity $UserToCopy | Select-Object sid
+
+# Copy the user profile from source machine to destination
+if (Test-Path "\\$RemoteComputer\c$\Users\") {
 
     # /XJ switch is important as it stops AppData infinite loop
     # More info: https://answers.microsoft.com/en-us/windows/forum/windows_7-files/windows-7-infinite-loop-while-using-robocopy/20f32f0c-4cb9-4125-923d-6a57e4d27232?auth=1
-    Robocopy.exe "C:\Users\$($Directory.Name)" "\\$RemoteComputer\c$\Users\$($Directory.Name)" /b /mir /mt /r:0 /w:0 /XJ /copyall
+    Robocopy.exe "C:\Users\$UserToCopy" "\\$RemoteComputer\c$\Users\$UserToCopy" /b /mir /mt /r:0 /w:0 /XJ /copyall
 
-} else { Write-Host "Unable to access: \\$RemoteComputer\c$\Users\$($Directory.Name)" }
+} else { Write-Host "Unable to access: \\$RemoteComputer\c$\Users\" }
+
+# Export the registry entry from the src computer
+reg.exe export "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$($UserSID.SID)" \\adcon.ad.fau.edu\c$\Users\TempUserProfile.reg
+# Create the new registry profile on the remote computer
+Invoke-Command $RemoteComputer { reg.exe import C:\Users\TempUserProfile.reg }
+# Remove the temporary registry key file from dst computer
+Invoke-Command $RemoteComputer { Remove-Item C:\Users\TempUserProfile.reg }
