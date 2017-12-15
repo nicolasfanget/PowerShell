a139aef8c9ff750c889739e85d3d2bfc30ba4e32
fhudzinski180 <33383891+fhudzinski180@users.noreply.github.com>
2017-12-15 14:15:58 -0500

    added SCOM\Flush-SCOMAgentCache.ps1

---
+++ SCOM/Flush-SCOMAgentCache.ps1
@@ -0,0 +1,10 @@
+﻿# This script flushes the scom agent cache
+
+# stop the agent health service
+Stop-Service HealthService
+
+# remove the cache
+Get-ChildItem -Recurse "C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State" | Remove-Item -Force -Recurse
+
+# start the agent health service
+Start-Service HealthService
