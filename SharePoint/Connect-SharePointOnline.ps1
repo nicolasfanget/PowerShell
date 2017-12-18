8ce6d6c98bae341b3a9225a824009fce0775800e
fhudzinski180 <33383891+fhudzinski180@users.noreply.github.com>
2017-12-18 09:25:25 -0500

    added SharePoint\Connect-SharePointOnline.ps1

---
+++ SharePoint/Connect-SharePointOnline.ps1
@@ -0,0 +1,10 @@
+﻿# Prompt for tenant admin URL
+$TenantAdminURL = Read-Host -Prompt "Enter tenant admin url"
+
+# Prompt for 365 Credentials
+$SPO_Credentials = Get-Credential -Message "Enter Office 365 Credentials"
+
+Connect-SPOService -Url $TenantAdminURL -Credential $SPO_Credentials
+
+# Make sure to disconnect session when done
+# Disconnect-SPOService
