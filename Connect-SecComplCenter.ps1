e960ae14c0fd286642720c5ea4c8d38373974f9a
fhudzinski180 <33383891+fhudzinski180@users.noreply.github.com>
2018-03-03 19:52:21 -0500

    edited Connect-SecComplCenter.ps1

--- Connect-SecComplCenter.ps1
+++ Connect-SecComplCenter.ps1
@@ -3,3 +3,6 @@ $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid -Credential $UserCredential -Authentication Basic -AllowRedirection 
 Import-PSSession $Session -AllowClobber -DisableNameChecking 
 $Host.UI.RawUI.WindowTitle = $UserCredential.UserName + " (Office 365 Security & Compliance Center)"
+
+# Make sure to run the cmdlet below to remove your session when you're done
+# Remove-PSSession $Session
