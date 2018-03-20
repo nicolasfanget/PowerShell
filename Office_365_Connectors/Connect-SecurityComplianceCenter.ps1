a87d2427660ff9327a0924a860e8a1d3cc8887e8
fhudzinski180 <33383891+fhudzinski180@users.noreply.github.com>
2018-03-20 08:39:46 -0400

    edited Connect-SecurityComplianceCenter.ps1
    changed the way credentials are created and added a warning at the bottom

--- Office_365_Connectors/Connect-SecurityComplianceCenter.ps1
+++ Office_365_Connectors/Connect-SecurityComplianceCenter.ps1
@@ -1,8 +1,20 @@
-﻿# Get login credentials
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid -Credential $UserCredential -Authentication Basic -AllowRedirection 
-Import-PSSession $Session -AllowClobber -DisableNameChecking 
-$Host.UI.RawUI.WindowTitle = $UserCredential.UserName + " (Office 365 Security & Compliance Center)"
+﻿<#
 
-# Make sure to run the cmdlet below to remove your session when you're done
-# Remove-PSSession $Session
+This script creates a remote powershell connection to Office 365 Security & Compliance center
+It uses an encrypted text file with the admin password
+
+Example: .\Connect-SecurityComplianceCenter.ps1
+
+#>
+
+# Create credential object
+$365Username = "it-fhudzinski@fau.onmicrosoft.com"
+$365Password = Get-Content ".\Office_365_Connectors\365_it-fhudzinski.txt" | ConvertTo-SecureString
+$365Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $365Username, $365Password
+
+# Create and import the remote powershell session
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid -Credential $365Credentials -Authentication Basic -AllowRedirection 
+Import-PSSession $Session -AllowClobber -DisableNameChecking 
+
+# Remind user to disconnect their session
+Write-Host -ForegroundColor Yellow "`n`nRemember to disconnect your remote session using: Remove-PSSession `$Session"
