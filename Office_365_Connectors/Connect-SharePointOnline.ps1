eb6f09e6c8fbdb4d08ede6f9d3ace656a4e02ec1
fhudzinski180 <33383891+fhudzinski180@users.noreply.github.com>
2018-04-05 13:51:57 -0400

    added login via encrypted password file to Connect-SharePointOnline.ps1

--- Office_365_Connectors/Connect-SharePointOnline.ps1
+++ Office_365_Connectors/Connect-SharePointOnline.ps1
@@ -1,10 +1,20 @@
-﻿# Prompt for tenant admin URL
-$TenantAdminURL = Read-Host -Prompt "Enter tenant admin url"
+﻿<#
 
-# Prompt for 365 Credentials
-$SPO_Credentials = Get-Credential -Message "Enter Office 365 Credentials"
+This script creates a remote powershell connection to Office 365 SharePoint Online
+It uses an encrypted text file with the admin password
 
-Connect-SPOService -Url $TenantAdminURL -Credential $SPO_Credentials
+Example: .\Connect-SharePointOnline.ps1
 
-# Make sure to disconnect session when done
-# Disconnect-SPOService
+#>
+
+$TenantAdminURL = "https://fau-admin.sharepoint.com/"
+
+# Create credential object
+$365Username = "it-fhudzinski@fau.onmicrosoft.com"
+$365Password = Get-Content ".\Office_365_Connectors\365_it-fhudzinski.txt" | ConvertTo-SecureString
+$365Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $365Username, $365Password
+
+Connect-SPOService -Url $TenantAdminURL -Credential $365Credentials
+
+# Remind user to disconnect their session
+Write-Host -ForegroundColor Yellow "`n`nRemember to disconnect your remote session using: Disconnect-SPOService"
