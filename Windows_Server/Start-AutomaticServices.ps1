a5609fdae65ecccd7ccd6f3141a95b30a390b80b
fhudzinski180 <33383891+fhudzinski180@users.noreply.github.com>
2018-04-05 09:48:41 -0400

    moved Start-AutomaticServices.ps1

--- Windows_Server/Start-AutomaticServices.ps1
+++ Windows_Server/Start-AutomaticServices.ps1
@@ -1,6 +1,7 @@
 <#
 This script looks for services that have a startup type of automatic that are not running and attempts to start them
 #>
+
 Get-CimInstance Win32_Service -Filter "startmode = 'auto' AND state != 'running'" | Select-Object Name, State | ForEach-Object {
     
     Start-Service $_.Name
